4942c4e32aa085c9d210277269e9c5e720561ba8
Stephanie KRUS <french@chezleskrus.com>
2018-01-07 22:38:39 +0000

    improve navbar. more to do git status

--- assets/js/myJavascript.js
+++ assets/js/myJavascript.js
@@ -1,3 +1,7 @@
 /**
  * Created by ninie on 07/01/2018.
  */
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the menu icon */
+function displayVerticalMenu() {
+  $("#navbar").toggleClass("responsive")
+}
